cd99c36c2c1a7b5ce28b306d25398b5dff5b3045
Waterswv <bmierke@gmail.com>
2019-11-18 22:47:08 -0700

    Successfully built component with tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
+import React from 'react'
 import Nav from '../components/nav'
+import { Chat } from '../components/chat'
 
 export default () => (
   <div>
     <Nav />
     <div className="hero">
-      <h1 className="title">Next.js + Tailwind css</h1>
+      <h1 className="title">Bryan Mierke circa 1983</h1>
+      <Chat />
     </div>
   </div>
 )
