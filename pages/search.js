42655302300cb6d3219859da7e40b67d4952cd33
Waterswv <bmierke@gmail.com>
2019-12-13 14:24:48 -0700

    testing Search page

--- pages/search.js
+++ pages/search.js
@@ -1,21 +1,20 @@
 import React from 'react'
 import fetch from 'isomorphic-unfetch'
 import withNavBar from '../components/withNavBar'
-const searchEndPt = 'https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty'
+const searchEndPt = 'https://bryanmierke.com/api/post'
 
 const SearchPage = ({ posts }) => {
   const [post, updatePosts] = React.useState(posts)
 
   console.log(posts)
   return (
-    <div>Search Coming Soon with posts by {post && post.by ? post.by : 'TBD'}</div>
+    <div>Search Coming Soon with posts like {post && post[0].postTitle ? post.postTitle : 'TBD'}</div>
   )
 }
 
 SearchPage.getInitialProps = async (context) => {
-console.log('hello')
-  const data = await fetch(searchEndPt)
-  const posts = await data.json()
+    const data = await fetch(searchEndPt)
+    const posts = await data.json()
 
   return { posts }
 }
