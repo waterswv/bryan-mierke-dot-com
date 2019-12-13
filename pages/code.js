af34ce298a5969121808bb4b8725c091437d190e
Waterswv <bmierke@gmail.com>
2019-12-13 14:24:38 -0700

    Update Code Route Page

--- pages/code.js
+++ pages/code.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import withNavBar from '../components/withNavBar'
 import Code from '../components/Code'
-let codes = ['cards-animation-example-chftk', 'dazzling-smoke-l7ghw', 'contact-component-nqqky']
+let codes = ['cards-animation-example-chftk', 'dazzling-smoke-l7ghw', 'contact-component-nqqky', 'dazzling-smoke-l7ghw', 'cards-animation-example-chftk', 'dazzling-smoke-l7ghw',]
 const AboutPage = () => (
-  <div className='w-11/12 bg-white-100 flex'>
+  <div className='w-11/12 bg-white-100 flex flex-wrap overflow-y-auto'>
     {codes.map(code => <Code key={code} id={code} />)}
   </div>
 )
