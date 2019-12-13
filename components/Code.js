8a2aacb8771f910b56fd9b0445d35b55889e8dcd
Waterswv <bmierke@gmail.com>
2019-12-13 14:24:23 -0700

    Break apart code examples page into func comps

--- components/Code.js
+++ components/Code.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Code ({ id }) {
+  const [loaded, load] = useState(false)
   return (
-    <div className='rounded p-4 w-2/6' style={{ height: '24rem' }}>
-      <embed className='rounded focus:outline-none focus:shadow-outline' width='100%' height='100%' src={`https://codesandbox.io/embed/${id}?codemirror=1&runonclick=1&view=editor&fontsize=14&hidenavigation=1&theme=dark`} />
+    <div className='rounded p-4 w-3/6 ' style={{ height: '24rem' }}>
+      {
+        !loaded
+          ? <Preview load={load} />
+          : <Embed id={id} />
+      }
     </div>
   )
 }
+
+const Preview = ({ load }) => (
+  <div className='relative flex w-full h-full bg-gray-300 rounded items-center justify-center' 
+    onClick={() => load(true)}>
+    <img className='w-full' src='https://screenshots.codesandbox.io/3fmbx.png' />
+    <PreviewText />
+  </div>
+)
+
+const Embed = ({ id }) => (
+  <embed className='w-full h-full rounded focus:outline-none focus:shadow-outline'
+    src={`https://codesandbox.io/embed/${id}?codemirror=1&runonclick=1&view=editor&fontsize=14&hidenavigation=1&theme=dark`} />
+)
+
+const PreviewText = ({ text }) => <div className='absolute bottom-0 text-center'>{text || 'Click to Load'}</div>
